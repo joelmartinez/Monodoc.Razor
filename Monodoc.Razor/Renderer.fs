8c7fef876d6b75f5426ea2ac3c34838f6418bb81
Joel Martinez <joelmartinez@gmail.com>
2016-07-13 11:50:40 -0700

    Added the renderer

---
+++ Monodoc.Razor/Renderer.fs
@@ -0,0 +1,11 @@
+﻿module public Renderer 
+    open System.Xml.Linq
+    open RazorEngine
+    open RazorEngine.Templating
+    let add (name:string) (template:string) = 
+        Engine.Razor.Compile(template, name, typeof<XDocument>)
+
+    let transform (name:string) (xml:string) =
+        let doc = XDocument.Parse(xml)
+
+        Engine.Razor.Run(name, typeof<XDocument>, doc)
