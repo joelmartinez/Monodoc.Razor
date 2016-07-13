83a651d01fda76a6fdbfd9b0baa6d2151f0ba5b2
Joel Martinez <joelmartinez@gmail.com>
2016-07-13 11:14:34 -0700

    Initial checkin - with razor dependency

---
+++ Monodoc.Razor/Component1.fs
@@ -0,0 +1,38 @@
+﻿namespace Monodoc.Razor
+
+    open System
+    open System.Collections.Generic
+    open System.Linq
+    open System.Xml.Linq
+    open System.Xml.XPath
+    open RazorEngine.Templating
+    open RazorEngine.Configuration
+
+    type XTRTemplateBase<'T>() =
+        inherit TemplateBase()
+
+        let templateMap = new Dictionary<string, Func<TemplateWriter>>()
+        [<DefaultValue>] val mutable public Model:'T
+        [<DefaultValue>] val mutable public currentContext:XElement 
+
+        override this.SetModel model =
+            this.Model <- model :?> 'T
+            base.SetModel(model)
+
+        member this.Match xpath delegateReference =
+            templateMap.Add(xpath, delegateReference)
+            ""
+        member this.getTemplates = templateMap
+
+        static member Initialize = 
+            let config = new TemplateServiceConfiguration()
+            config.Namespaces <- new HashSet<string>([|"Monodoc.Razor"|])
+
+            let service = RazorEngineService.Create(config)
+            RazorEngine.Engine.Razor <- service
+
+    type XTRTemplateBase() = 
+        inherit XTRTemplateBase<XDocument>()
+
+        let join (seq:IEnumerable<string>) = System.String.Join(Environment.NewLine, seq |> Seq.toArray)
+           
