2369197a1bc47655de3d1091113e72b327eaf057
Joel Martinez <joelmartinez@gmail.com>
2016-07-25 17:39:47 -0400

    Initial renderer and test implementation

---
+++ Monodoc.Razor.Tests/Test.fs
@@ -0,0 +1,13 @@
+﻿namespace Monodoc.Razor.Tests
+open System
+open NUnit.Framework
+open Monodoc.Razor
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member x.TestCase() =
+        Renderer.add
+        Assert.IsTrue(false)
+
