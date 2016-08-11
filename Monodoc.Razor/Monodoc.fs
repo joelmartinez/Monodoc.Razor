4fcd533f07ad31adc5efa5099b385999764fb6b9
Joel Martinez <joelmartinez@gmail.com>
2016-08-11 15:11:19 -0400

    initial barebones implementation for RazorGenerator

---
+++ Monodoc.Razor/Monodoc.fs
@@ -0,0 +1,8 @@
+﻿namespace Monodoc.Razor 
+    open Monodoc
+    open System.Collections.Generic
+
+    type RazorGenerator() =
+        interface IDocGenerator<string> with
+            member this.Generate ((hs:HelpSource), (internalId:string), (context:Dictionary<string, string>)) =
+                hs.GetText internalId
