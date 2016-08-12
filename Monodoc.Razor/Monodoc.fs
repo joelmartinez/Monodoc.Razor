00a301b8684ae1f89d51634bee7255fff786ee29
Joel Martinez <joelmartinez@gmail.com>
2016-08-12 16:55:13 -0400

    Initialize razor and execute template in renderer. closes #11

--- Monodoc.Razor/Monodoc.fs
+++ Monodoc.Razor/Monodoc.fs
@@ -1,8 +1,27 @@
 ﻿namespace Monodoc.Razor 
     open Monodoc
     open System.Collections.Generic
+    open System.Xml
+    open System.Xml.Linq
 
-    type RazorGenerator() =
+
+
+    type public RazorGenerator =
+
+
+        new () =
+            RazorTemplateBase.Initialize
+            new RazorGenerator()
+
+
         interface IDocGenerator<string> with
             member this.Generate ((hs:HelpSource), (internalId:string), (context:Dictionary<string, string>)) =
-                hs.GetText internalId
+
+                let findTemplate (id:string) =
+                    if id.Contains("T:") then "type" else "namespace"
+
+                let source = hs.GetText internalId
+
+                Renderer.transform (findTemplate internalId) source
+
+
