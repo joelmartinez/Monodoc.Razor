1bd351a392ea8f03f755c1003627f7e38db9196b
Joel Martinez <joelmartinez@gmail.com>
2016-08-29 17:23:33 -0400

    renders member list

--- Monodoc.Razor.Tests/Test.fs
+++ Monodoc.Razor.Tests/Test.fs
@@ -104,3 +104,16 @@ type Test() =
         let rendered = tree.RenderUrl("N:My.Sample", generator); 
 
         Assert.AreEqual(true, rendered.Contains("SomeClass"));
+
+    [<Test>]
+    member x.FileTypeMemberList() =
+        let generator = getGenerator()
+
+        let path = "../../../Templates/typeoverview.cshtml"
+        let template = File.ReadAllText(path);
+
+        generator.Add Templates.Type template
+
+        let rendered = tree.RenderUrl("T:My.Sample.SomeClass", generator); 
+
+        Assert.AreEqual(true, rendered.Contains("GetName"));
