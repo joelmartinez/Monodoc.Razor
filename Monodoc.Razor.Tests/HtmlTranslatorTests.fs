3c59e64c0f8463e1396afb126f81482fa838c6e3
Joel Martinez <joelmartinez@gmail.com>
2016-08-30 17:08:26 -0400

    html translation for text nodes

---
+++ Monodoc.Razor.Tests/HtmlTranslatorTests.fs
@@ -0,0 +1,19 @@
+﻿namespace Monodoc.Razor.Tests
+    open System
+    open System.Collections.Generic
+
+    open System.Xml.Linq
+
+    open NUnit.Framework
+    open Monodoc.Razor.HtmlTranslator
+
+    [<TestFixture>]
+    type HtmlTranslatorTests() = 
+
+        [<Test>]
+        member x.Text() =
+            let doc = XDocument.Parse("<remarks>This is some text</remarks>")
+
+            let actual = html doc.Root
+
+            Assert.AreEqual ("This is some text", actual)
--- Monodoc.Razor/HtmlTranslator.fs
+++ Monodoc.Razor/HtmlTranslator.fs
@@ -1,9 +1,24 @@
 ﻿namespace Monodoc.Razor
     open System
+    open System.Text
+    open System.Linq
     open System.Xml
     open System.Xml.Linq
     open System.Xml.XPath
 
     module HtmlTranslator =
-    
-        let html (doc:XElement)  = doc.ToString()
+        let rec render (doc:XObject) (sb:StringBuilder) =
+            match doc with
+            | :? XElement as e -> 
+                match e.Name.LocalName with
+                // TODO: include logic for individual constructs in ecma XML
+                | _ -> e.Nodes() |> Seq.iter (fun d -> render d sb)
+
+            | :? XText as t -> sb.Append(t.Value) |> ignore 
+            | _ -> failwith "unknown node type"
+
+        let html (doc:XElement)  = 
+            let sb = StringBuilder()
+            render doc sb
+
+            sb.ToString()
