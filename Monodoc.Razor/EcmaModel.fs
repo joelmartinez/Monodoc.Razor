0e3dce5e32c17d26c04c31d2e5589567fd31d3f1
Joel Martinez <joelmartinez@gmail.com>
2016-08-30 10:00:53 -0400

    Moved `EcmaModel` to own file.

---
+++ Monodoc.Razor/EcmaModel.fs
@@ -0,0 +1,19 @@
+﻿namespace Monodoc.Razor
+    open System
+    open System.Collections.Generic
+    open System.Linq
+    open System.Xml
+    open System.Xml.Linq
+    open System.Xml.XPath
+
+
+    type EcmaModel(doc:XDocument, context:Dictionary<string,string>, xpath:string) =
+        let index = match context.["show"] with
+                        | "member" -> Int32.Parse context.["index"]
+                        | _ -> 0
+
+        member this.Source = doc
+        member this.Context = context
+        member this.XPath = xpath
+
+        member this.Doc with get () = this.Source.XPathSelectElements(this.XPath).Skip(index).First()
--- Monodoc.Razor/RazorTemplating.fs
+++ Monodoc.Razor/RazorTemplating.fs
@@ -9,16 +9,6 @@
     open RazorEngine.Configuration
 
 
-    type EcmaModel(doc:XDocument, context:Dictionary<string,string>, xpath:string) =
-        let index = match context.["show"] with
-                        | "member" -> Int32.Parse context.["index"]
-                        | _ -> 0
-
-        member this.Source = doc
-        member this.Context = context
-        member this.XPath = xpath
-
-        member this.Doc with get () = this.Source.XPathSelectElements(this.XPath).Skip(index).First()
 
     type RazorTemplateBase<'T>() =
         inherit TemplateBase()
