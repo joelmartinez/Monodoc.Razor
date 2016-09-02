58e0ddade4ece4b76f03bc6efd4d25889773d629
Joel Martinez <joelmartinez@gmail.com>
2016-09-02 11:25:49 -0400

    added Html helper to template base

--- Monodoc.Razor/RazorTemplating.fs
+++ Monodoc.Razor/RazorTemplating.fs
@@ -10,21 +10,39 @@
 
 
 
-    type RazorTemplateBase<'T>() =
+    type RazorTemplateBase<'T when 'T :> EcmaModel>() =
         inherit TemplateBase()
 
         let templateMap = new Dictionary<string, Func<TemplateWriter>>()
         [<DefaultValue>] val mutable public Model:'T
         [<DefaultValue>] val mutable public currentContext:XElement 
 
+        member this.Html = RazorHtmlHelper(this)
         override this.SetModel model =
             this.Model <- model :?> 'T
             base.SetModel(model)
+
+        override this.ResolveLayout(name:string) =
+            let l = base.ResolveLayout(name) 
+            let ecmaModel = l :?> RazorTemplateBase<EcmaModel>
+            ecmaModel.Model <- this.Model
+            l
+
 
         member this.Match xpath delegateReference =
             templateMap.Add(xpath, delegateReference)
             ""
         member this.getTemplates = templateMap
+
+
+    and public RazorHtmlHelper<'T when 'T :> EcmaModel>(template:RazorTemplateBase<'T>) =
+        member this.Template = template
+
+        member this.RenderPartial (templateName:string) =
+            this.Template.Include(templateName, this.Template.Model)
+
+        member this.RenderPartial (templateName:string, model:Object) =
+            this.Template.Include(templateName, model)
 
 
     type public RazorTemplateBase() = 
@@ -38,3 +56,5 @@
 
             let service = RazorEngineService.Create(config)
             service
+
+
